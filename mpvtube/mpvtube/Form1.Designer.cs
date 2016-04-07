0f802d87ea7246fff86c64bf29512df418f16136
majortriad <ajone241@gmail.com>
2016-04-07 16:39:54 +1000

    Initial framework. Searching and playing is available

--- mpvtube/mpvtube/Form1.Designer.cs
+++ mpvtube/mpvtube/Form1.Designer.cs
@@ -28,12 +28,273 @@
 		/// </summary>
 		private void InitializeComponent ()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.searchTxt = new System.Windows.Forms.TextBox();
+			this.searchBtn = new System.Windows.Forms.Button();
+			this.qualityBox = new System.Windows.Forms.GroupBox();
+			this.audioQuality = new System.Windows.Forms.RadioButton();
+			this.lowQuality = new System.Windows.Forms.RadioButton();
+			this.highQuality = new System.Windows.Forms.RadioButton();
+			this.playBtn = new System.Windows.Forms.Button();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.signInToYouTubeNAToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.alwaysOnTopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.closeAfterStartingVideoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.resultsList = new System.Windows.Forms.ListView();
+			this.titleCol = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.uploaderCol = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.dateTimeCol = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.descriptCol = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.enterURLToPlayToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.qualityBox.SuspendLayout();
+			this.menuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// searchTxt
+			// 
+			this.searchTxt.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.searchTxt.Location = new System.Drawing.Point(12, 27);
+			this.searchTxt.Name = "searchTxt";
+			this.searchTxt.Size = new System.Drawing.Size(629, 20);
+			this.searchTxt.TabIndex = 0;
+			// 
+			// searchBtn
+			// 
+			this.searchBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.searchBtn.Location = new System.Drawing.Point(647, 27);
+			this.searchBtn.Name = "searchBtn";
+			this.searchBtn.Size = new System.Drawing.Size(75, 23);
+			this.searchBtn.TabIndex = 1;
+			this.searchBtn.Text = "Search";
+			this.searchBtn.UseVisualStyleBackColor = true;
+			this.searchBtn.Click += new System.EventHandler(this.searchBtn_Click);
+			// 
+			// qualityBox
+			// 
+			this.qualityBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.qualityBox.Controls.Add(this.audioQuality);
+			this.qualityBox.Controls.Add(this.lowQuality);
+			this.qualityBox.Controls.Add(this.highQuality);
+			this.qualityBox.Location = new System.Drawing.Point(12, 53);
+			this.qualityBox.Name = "qualityBox";
+			this.qualityBox.Size = new System.Drawing.Size(629, 46);
+			this.qualityBox.TabIndex = 3;
+			this.qualityBox.TabStop = false;
+			this.qualityBox.Text = "Quality";
+			// 
+			// audioQuality
+			// 
+			this.audioQuality.AutoSize = true;
+			this.audioQuality.Enabled = false;
+			this.audioQuality.Location = new System.Drawing.Point(117, 19);
+			this.audioQuality.Name = "audioQuality";
+			this.audioQuality.Size = new System.Drawing.Size(103, 17);
+			this.audioQuality.TabIndex = 2;
+			this.audioQuality.TabStop = true;
+			this.audioQuality.Text = "Audio only (N/A)";
+			this.audioQuality.UseVisualStyleBackColor = true;
+			// 
+			// lowQuality
+			// 
+			this.lowQuality.AutoSize = true;
+			this.lowQuality.Location = new System.Drawing.Point(61, 19);
+			this.lowQuality.Name = "lowQuality";
+			this.lowQuality.Size = new System.Drawing.Size(49, 17);
+			this.lowQuality.TabIndex = 1;
+			this.lowQuality.TabStop = true;
+			this.lowQuality.Text = "360p";
+			this.lowQuality.UseVisualStyleBackColor = true;
+			// 
+			// highQuality
+			// 
+			this.highQuality.AutoSize = true;
+			this.highQuality.Location = new System.Drawing.Point(6, 19);
+			this.highQuality.Name = "highQuality";
+			this.highQuality.Size = new System.Drawing.Size(49, 17);
+			this.highQuality.TabIndex = 0;
+			this.highQuality.TabStop = true;
+			this.highQuality.Text = "720p";
+			this.highQuality.UseVisualStyleBackColor = true;
+			// 
+			// playBtn
+			// 
+			this.playBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.playBtn.Enabled = false;
+			this.playBtn.Location = new System.Drawing.Point(647, 56);
+			this.playBtn.Name = "playBtn";
+			this.playBtn.Size = new System.Drawing.Size(75, 43);
+			this.playBtn.TabIndex = 4;
+			this.playBtn.Text = "Play";
+			this.playBtn.UseVisualStyleBackColor = true;
+			this.playBtn.Click += new System.EventHandler(this.playBtn_Click);
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.settingsToolStripMenuItem,
+            this.helpToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(734, 24);
+			this.menuStrip1.TabIndex = 5;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// fileToolStripMenuItem
+			// 
+			this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.signInToYouTubeNAToolStripMenuItem,
+            this.enterURLToPlayToolStripMenuItem});
+			this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+			this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+			this.fileToolStripMenuItem.Text = "File";
+			// 
+			// signInToYouTubeNAToolStripMenuItem
+			// 
+			this.signInToYouTubeNAToolStripMenuItem.Enabled = false;
+			this.signInToYouTubeNAToolStripMenuItem.Name = "signInToYouTubeNAToolStripMenuItem";
+			this.signInToYouTubeNAToolStripMenuItem.Size = new System.Drawing.Size(208, 22);
+			this.signInToYouTubeNAToolStripMenuItem.Text = "Sign in to YouTube (N/A)";
+			// 
+			// settingsToolStripMenuItem
+			// 
+			this.settingsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.alwaysOnTopToolStripMenuItem,
+            this.closeAfterStartingVideoToolStripMenuItem});
+			this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+			this.settingsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+			this.settingsToolStripMenuItem.Text = "Settings";
+			// 
+			// alwaysOnTopToolStripMenuItem
+			// 
+			this.alwaysOnTopToolStripMenuItem.CheckOnClick = true;
+			this.alwaysOnTopToolStripMenuItem.Name = "alwaysOnTopToolStripMenuItem";
+			this.alwaysOnTopToolStripMenuItem.Size = new System.Drawing.Size(205, 22);
+			this.alwaysOnTopToolStripMenuItem.Text = "Always on top";
+			// 
+			// closeAfterStartingVideoToolStripMenuItem
+			// 
+			this.closeAfterStartingVideoToolStripMenuItem.CheckOnClick = true;
+			this.closeAfterStartingVideoToolStripMenuItem.Name = "closeAfterStartingVideoToolStripMenuItem";
+			this.closeAfterStartingVideoToolStripMenuItem.Size = new System.Drawing.Size(205, 22);
+			this.closeAfterStartingVideoToolStripMenuItem.Text = "Close after starting video";
+			// 
+			// helpToolStripMenuItem
+			// 
+			this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+			this.helpToolStripMenuItem.Enabled = false;
+			this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+			this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+			this.helpToolStripMenuItem.Text = "Help";
+			// 
+			// aboutToolStripMenuItem
+			// 
+			this.aboutToolStripMenuItem.Enabled = false;
+			this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+			this.aboutToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+			this.aboutToolStripMenuItem.Text = "About...";
+			// 
+			// resultsList
+			// 
+			this.resultsList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.resultsList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.titleCol,
+            this.uploaderCol,
+            this.dateTimeCol,
+            this.descriptCol});
+			this.resultsList.Location = new System.Drawing.Point(12, 105);
+			this.resultsList.MultiSelect = false;
+			this.resultsList.Name = "resultsList";
+			this.resultsList.Size = new System.Drawing.Size(710, 344);
+			this.resultsList.TabIndex = 6;
+			this.resultsList.UseCompatibleStateImageBehavior = false;
+			this.resultsList.View = System.Windows.Forms.View.Details;
+			this.resultsList.SelectedIndexChanged += new System.EventHandler(this.resultsList_SelectedIndexChanged);
+			this.resultsList.DoubleClick += new System.EventHandler(this.resultsList_DoubleClick);
+			// 
+			// titleCol
+			// 
+			this.titleCol.Text = "Title";
+			this.titleCol.Width = 300;
+			// 
+			// uploaderCol
+			// 
+			this.uploaderCol.Text = "Uploader";
+			this.uploaderCol.Width = 100;
+			// 
+			// dateTimeCol
+			// 
+			this.dateTimeCol.Text = "Time uploaded";
+			this.dateTimeCol.Width = 150;
+			// 
+			// descriptCol
+			// 
+			this.descriptCol.Text = "Desciption";
+			this.descriptCol.Width = 1000;
+			// 
+			// enterURLToPlayToolStripMenuItem
+			// 
+			this.enterURLToPlayToolStripMenuItem.Name = "enterURLToPlayToolStripMenuItem";
+			this.enterURLToPlayToolStripMenuItem.Size = new System.Drawing.Size(208, 22);
+			this.enterURLToPlayToolStripMenuItem.Text = "Enter URL to play";
+			// 
+			// Form1
+			// 
+			this.AcceptButton = this.searchBtn;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "Form1";
+			this.ClientSize = new System.Drawing.Size(734, 461);
+			this.Controls.Add(this.resultsList);
+			this.Controls.Add(this.playBtn);
+			this.Controls.Add(this.qualityBox);
+			this.Controls.Add(this.searchBtn);
+			this.Controls.Add(this.searchTxt);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
+			this.MinimumSize = new System.Drawing.Size(750, 500);
+			this.Name = "Form1";
+			this.Text = "MPVTube";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.qualityBox.ResumeLayout(false);
+			this.qualityBox.PerformLayout();
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.TextBox searchTxt;
+		private System.Windows.Forms.Button searchBtn;
+		private System.Windows.Forms.GroupBox qualityBox;
+		private System.Windows.Forms.RadioButton lowQuality;
+		private System.Windows.Forms.RadioButton highQuality;
+		private System.Windows.Forms.RadioButton audioQuality;
+		private System.Windows.Forms.Button playBtn;
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem alwaysOnTopToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem closeAfterStartingVideoToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem signInToYouTubeNAToolStripMenuItem;
+		private System.Windows.Forms.ListView resultsList;
+		private System.Windows.Forms.ColumnHeader titleCol;
+		private System.Windows.Forms.ColumnHeader uploaderCol;
+		private System.Windows.Forms.ColumnHeader dateTimeCol;
+		private System.Windows.Forms.ColumnHeader descriptCol;
+		private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem enterURLToPlayToolStripMenuItem;
 	}
 }
 
--- mpvtube/mpvtube/Form1.cs
+++ mpvtube/mpvtube/Form1.cs
@@ -8,13 +8,112 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Upload;
+using Google.Apis.Util.Store;
+using Google.Apis.YouTube.v3;
+using Google.Apis.YouTube.v3.Data;
+
+
 namespace mpvtube
 {
 	public partial class Form1 : Form
 	{
+		public List<string> urls;
+
+		public const string MpvRoot = "mpv";
+		public const string QualRoot = "ytdl-format=";
+
+		public const string QualBest = "best";
+		public const string QualLow = "\"best[height=360]\"";
+		public const string QualAudio = "bestaudio";
+
 		public Form1 ()
 		{
 			InitializeComponent();
+		}
+
+		private void Form1_Load (object sender, EventArgs e)
+		{
+			highQuality.Checked = true;
+		}
+
+		private async void searchBtn_Click (object sender, EventArgs e)
+		{
+			resultsList.Items.Clear();
+			try
+			{
+				urls.Clear();
+			}
+			catch { }
+			var youtubeService = new YouTubeService(new BaseClientService.Initializer()
+			{
+				ApiKey = Properties.Resources.ApiKey.ToString(),
+				ApplicationName = this.GetType().ToString()
+			});
+
+			var searchListRequest = youtubeService.Search.List("snippet");
+			searchListRequest.Q = searchTxt.Text;
+			searchListRequest.MaxResults = 50;
+
+			var searchListResponse = await searchListRequest.ExecuteAsync();
+
+			urls = new List<string>();
+
+			foreach (var searchResult in searchListResponse.Items)
+			{
+				if (Equals(searchResult.Id.Kind, "youtube#video"))
+				{
+					urls.Add(searchResult.Id.VideoId);
+
+					ListViewItem itemToAdd = new ListViewItem();
+					itemToAdd.Text = searchResult.Snippet.Title;
+					itemToAdd.SubItems.Add(new ListViewItem.ListViewSubItem(itemToAdd, searchResult.Snippet.ChannelTitle));
+					itemToAdd.SubItems.Add(new ListViewItem.ListViewSubItem(itemToAdd, searchResult.Snippet.PublishedAt.ToString()));
+					itemToAdd.SubItems.Add(new ListViewItem.ListViewSubItem(itemToAdd, searchResult.Snippet.Description));
+					resultsList.Items.Add(itemToAdd);
+				}
+			}
+		}
+
+		private void playVideo(string url, string quality)
+		{
+			if (alwaysOnTopToolStripMenuItem.Checked)
+				quality = quality + " --ontop";
+			System.Diagnostics.Process process = new System.Diagnostics.Process();
+			System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+			startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+			startInfo.FileName = "cmd.exe";
+			startInfo.Arguments = "/K mpv https://www.youtube.com/watch?v=" + url + " --ytdl-format=" + quality;
+			Console.Write(startInfo.Arguments);
+			process.StartInfo = startInfo;
+			process.Start();
+			if (closeAfterStartingVideoToolStripMenuItem.Checked)
+				Application.Exit();
+		}
+
+		private void playBtn_Click (object sender, EventArgs e)
+		{
+			resultsList_DoubleClick(sender, e);
+		}
+
+		private void resultsList_DoubleClick (object sender, EventArgs e)
+		{
+			if (highQuality.Checked)
+				playVideo(urls[resultsList.FocusedItem.Index], QualBest);
+			else if (lowQuality.Checked)
+				playVideo(urls[resultsList.FocusedItem.Index], QualLow);
+			else if (audioQuality.Checked)
+				playVideo(urls[resultsList.FocusedItem.Index], QualAudio);
+		}
+
+		private void resultsList_SelectedIndexChanged (object sender, EventArgs e)
+		{
+			if (resultsList.SelectedItems != null)
+				playBtn.Enabled = true;
+			else
+				playBtn.Enabled = false;
 		}
 	}
 }
